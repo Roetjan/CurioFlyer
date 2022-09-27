5ffcd7eeacb3ef14011b79052be0c08557572a58
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-09-27 11:08:12 +0200

    script toegevoegd voor de vakken. dat ze interactive zijn

---
+++ script.js
@@ -0,0 +1,15 @@
+function Pro(){
+    document.getElementById("subTxt").innerHTML = "<p>amet</p>";
+}
+
+function Pra(){
+    document.getElementById("subTxt").innerHTML = "<p>dolor</p>";
+}
+
+function Web(){
+    document.getElementById("subTxt").innerHTML = "<p>ipsum</p>";
+}
+
+function Native(){
+    document.getElementById("subTxt").innerHTML = "<p>lorem</p>";
+}
