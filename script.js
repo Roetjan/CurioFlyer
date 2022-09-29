396794223a1dfd1b5fe43b7e2d77a431bf51d1f6
Roetjan <93587343+Roetjan@users.noreply.github.com>
2022-09-29 13:23:02 +0200

    de teksten met responsive buttons toegevoegd bij de vakken

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function Pro(){
-    document.getElementById("subTxt").innerHTML = "<p>amet</p>";
+    document.getElementById("subTxt").innerHTML = "<p>Bij PRO krijg je informatie over hoe je moet werken als software developer en welke methodes je moet toepassen, je krijgt hier ook dan opdrachten om een voorbeeld planning te maken voor je projecten die je bij PRA gaat maken.</p>";
 }
 
 function Pra(){
-    document.getElementById("subTxt").innerHTML = "<p>dolor</p>";
+    document.getElementById("subTxt").innerHTML = "<p>Bij PRA krijg je iedere 3-9 weken een nieuw project waar je dan meestal in een groepje aan gaat werken. De projecten sluiten aan bij de dingen die je leert bij NATIVE en WEB</p>";
 }
 
 function Web(){
-    document.getElementById("subTxt").innerHTML = "<p>ipsum</p>";
+    document.getElementById("subTxt").innerHTML = "<p>Bij WEB ga je te werk met websites, je gaat leren hoe je een website moet maken en hoe je het mooier kan maken met styling oftwel de leuke plaatjes en kleuren die je op elke site tezien krijgt.</p>";
 }
 
 function Native(){
-    document.getElementById("subTxt").innerHTML = "<p>lorem</p>";
+    document.getElementById("subTxt").innerHTML = "<p>Bij Native maak je vooral console apps, dit zijn in het eerste deel van de opleiding vooral console apps in de Python programmeertaal. Je begint hierbij heel simpel met micro-bit. Daarna stap je over naar het maken van de console apps in python</p>";
 }
